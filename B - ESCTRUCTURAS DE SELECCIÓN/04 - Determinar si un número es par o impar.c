4e86b4f163453f616b9505d7350d696739faf27d
Rodrigo Vegas <r.vegasjimenez@gmail.com>
2019-06-30 16:02:25 +0200

    Updated

--- B - ESCTRUCTURAS DE SELECCIÓN/04 - Determinar si un número es par o impar.c
+++ B - ESCTRUCTURAS DE SELECCIÓN/04 - Determinar si un número es par o impar.c
@@ -1,11 +1,26 @@
-//Con este ejercicio vamos a calcular la factura de la luz según el gasto de corriente eléctrica. Para un gasto menor de 1.000Kwh la tarifa es 1.2, entre 1.OOO y 1.850Kwh es 1.0 y mayor de 1.85OKwh 0.9.
+//Con este ejercicio crearemos una herramienta que determina si un número introducido por el usuario es par o impar.
 
 #include <stdio.h>			//Existen muchos tipos de librerías, las cuales nos otorgan la capacidad de realizar funciones, y se añaden precedidas por #include. "stdio" significa "standard input output" y ".h" significa "header". Nos sirve para poder poner cosas en pantalla y guardar datos.
 
 int main(){					//Esta es la función principal (main) que vamos a crear. "Int" quiere decir que es un número entero.
 	
-
-
-	return 0;							//Gracias a poner return 0; podemos saber que la función ha terminado correctamente, y que nuestro programa no ha fallado en un punto a mitad de ejecución. Es una buena costumbre utilizarlo.
+	int n1;					//Declaramos la variable n1, un número que será entero y que el usuario tecleará.
+	
+	printf("Introduce un número: ");	//Parte textual en la que solicitamos al usuario que introduzca un número.
+	
+	scanf("%i",&n1);		//Mediante scanf, permitimos que el usuario sea capaz de introducir dicho número entero (%i), y guardarlo como n1 (&n1).
+	
+	if (n1 % 2 == 0){		//Mediante if, le decimos al programa que si ese número (n1) es divisible entre 2 (% 2), y el resultado es 0 (== 0), entonces el número es par.
+		
+		printf("El número %i es par.",n1);		//Parte textual en la que confirmamos al usuario que el número es par.
+		
+	}
+	
+	else {					//Mediante else, le decimos al programa que todo lo que no sea dividible entre dos, sea impar.
+		
+		printf("El número %i es impar.",n1);	//Parte textual en la que confirmamos al usuario que el número es impar.
+	}
+	
+	return 0;				//Gracias a poner return 0; podemos saber que la función ha terminado correctamente, y que nuestro programa no ha fallado en un punto a mitad de ejecución. Es una buena costumbre utilizarlo.
 
 }
