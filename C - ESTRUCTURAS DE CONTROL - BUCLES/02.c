333131ea5e282a6f6d24cf187f358b9ffea6294e
Rodrigo Vegas <r.vegasjimenez@gmail.com>
2019-07-08 17:26:22 +0200

    Updated

--- C - ESTRUCTURAS DE CONTROL - BUCLES/02.c
+++ C - ESTRUCTURAS DE CONTROL - BUCLES/02.c
@@ -4,20 +4,24 @@
 
 int main (){            //Esta es la función principal (main) que vamos a crear. "Int" quiere decir que es un número entero.
 
-  int contador, n1, suma=0;
+  int contador, n1, suma=0;     //Declaramos las variables que vamos a necesitar utilizar, en este caso el valor del contador (1), el número que pedimos (n1), y el valor inicializado de la suma, que es 0.
 
-  printf("Introduce la cantidad de números a sumar: ");
-  scanf("%i",&n1);
+  printf("Introduce la cantidad de números a sumar: ");   //Parte textual en la que pedimos al usuario que introduzca los números que quiere sumar.
 
-  contador = 1;
+  scanf("%i",&n1);      //Mediante scanf permitimos que el usuario pueda introducir un número entero (n1), y guardarlo como n1 (&n1).
 
-  while (contador <= n1) {
-    suma = suma + contador;
-    contador = contador + 1;
+  contador = 1;         //Valor de incremento del contador.
+
+  while (contador <= n1) {      //Mediante while, hacemos que el programa repita esta función hasta que la condición (contador <= n1) no se cumpla.
+
+    suma = suma + contador;     //Función que calcula el número que se va a imprimir en pantalla.
+
+    contador = contador + 1;    //Función que le dice al programa la cantidad de veces que se ha repetido el bucle.
+
   }
 
-  printf("%i",suma);
+  printf("%i",suma);    //Parte textual en la que mostramos el resultado final.
 
 
-  return 0;
-}
+  return 0;             //Gracias a poner return 0; podemos saber que la función ha terminado correctamente, y que nuestro programa no ha fallado en un punto a mitad de ejecución. Es una buena costumbre utilizarlo.
+  }
